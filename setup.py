cdec23b7129cd01a33d30afed6566336e0bf45e1
jlgoldb2 <jlgoldb2@asu.edu>
2014-12-08 14:16:05 -0700

    added setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='redsugar',
+    version='0.0.1a',
+    packages=[
+        'redsugar'
+    ],
+    url='https://github.com/jlgoldb2/redsugar',
+    license='MIT',
+    author='Jason Goldberger',
+    author_email='jlgoldb2@asu.edu',
+    include_package_data=True,
+    platforms='any',
+    description='Side project'
+)
