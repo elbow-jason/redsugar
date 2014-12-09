7ce8c9b5e0719b1dd839b30f1e40486cd316ae09
jlgoldb2 <jlgoldb2@asu.edu>
2014-12-09 11:38:36 -0700

    added static source code

---
+++ test/testing_source_code.py
@@ -0,0 +1,15 @@
+
+TESTING_SOURCE_CODE = """
+def hello
+    print "hello world"
+end
+
+def noway
+    def what
+        fncrap = 1
+        bend = true
+        indef = false
+    end
+    return
+end
+"""
