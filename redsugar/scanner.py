0cf7f160720f4f91344ba763d10a231f3efcfede
jlgoldb2 <jlgoldb2@asu.edu>
2014-12-09 00:09:18 -0700

    scanner has tests

--- redsugar/scanner.py
+++ redsugar/scanner.py
@@ -3,6 +3,8 @@ class RedScanner(object):
 
     def __init__(self, text):
         self.text = text
+        self.position = 0
+        self.marker = 0
 
     def start(self):
         pass
--- test/test_red_scanner_class.py
+++ test/test_red_scanner_class.py
@@ -25,6 +25,46 @@ class TestRedScanner(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def test_start(self):
-        first_char = self.scanner.start()
-        self.assertEqual(first_char, 'f')
+    def test_starts_at_position_0(self):
+        self.assertEqual(self.scanner.position, 0)
+
+    def test_next(self):
+        self.scanner.position = 1
+        self.scanner.next()
+        self.assertEqual(self.scanner.position, 2)
+
+    def test_prev(self):
+        self.scanner.position = 2
+        self.scanner.prev()
+        self.assertEqual(self.scanner.position, 1)
+
+    def test_get(self):
+        self.scanner.position = 1
+        current_char = self.scanner.get()
+        self.assertEqual(current_char, 'n')
+
+    def test_mark(self):
+        self.scanner.position = 2
+        self.scanner.mark()
+        self.assertEqual(self.scanner.marker, 2)
+
+    def test_diff(self):
+        self.scanner.marker = 6
+        self.scanner.position = 2
+        delta = self.diff()
+        self.assertEqual(delta, -4)
+        self.scanner.position = 12
+        delta = self.diff()
+        self.assertEqual(delta, 6)
+
+    def test_goto(self):
+        self.scanner.goto(3)
+        self.assertEqual(self.scanner.position, 3)
+
+    def test_find_next(self):
+        self.scanner.text = """Jason Louis"""
+        self.scanner.position = 3
+        should_be_o = self.scanner.get()
+        should_be_7 = self.scanner.find_next('o')
+        self.assertEqual(should_be_o, 'o')
+        self.assertEqual(should_be_7, 7)
