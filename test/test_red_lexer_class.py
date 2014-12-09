a6332e4e79dbad672748f0b92e0f937e6f3e4901
jlgoldb2 <jlgoldb2@asu.edu>
2014-12-08 19:45:55 -0700

    split tests up to specific functionality

--- test/test_red_lexer_class.py
+++ test/test_red_lexer_class.py
@@ -3,26 +3,46 @@ import unittest
 
 from redsugar.utils import RedLexer
 
+TESTING_SOURCE_CODE = """
+fn hello
+    print "hello world"
+end
+"""
+
 
 class TestRedLexer(unittest.TestCase):
 
     def setUp(self):
-        self.source_code = """fn hello
-print "hello world"
-end
-        """
+        self.source_code = TESTING_SOURCE_CODE
         self.lexer = RedLexer(self.source_code)
+        self.lexer.tokenize()
 
     def tearDown(self):
         pass
 
-    def test_tokenize(self):
-        self.lexer.tokenize()
-        self.assertEqual(self.lexer.tokens[0], '\n')
-        self.assertEqual(self.lexer.tokens[2], 'fn')
+    def test_tokenize_makes_list(self):
+        self.assertIs(list, type(self.lexer.tokens))
+
+    def test_tokenize_removes_newlines(self):
+        for item in self.lexer.tokens:
+            self.assertNotIn('\n', item)
+
+    def test_tokenize_content_is_as_expected(self):
+        self.assertEqual(self.lexer.tokens[0], 'fn')
+        self.assertEqual(self.lexer.tokens[1], 'hello')
+        self.assertEqual(self.lexer.tokens[2], 'print')
+        self.assertEqual(
+            self.lexer.tokens[3],
+            '"',
+            "first single quote not alone"
+        )
+        self.assertEqual(self.lexer.tokens[4], "hello")
+        self.assertEqual(
+            self.lexer.tokens[3],
+            '"',
+            "second single quote not alone"
+        )
+        self.assertEqual(self.lexer.tokens[2], 'end')
 
     def test_remove_empty_tokens(self):
         self.assertNotIn('', self.lexer.tokens)
-
-    def test_remove_newlines(self):
-        self.assertNotIn('\n', self.lexer.tokens)
